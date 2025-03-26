c543b8088cafbe9ef800c902b5523dba4e36da1a
marcomaciel04 <marco.maciel@csacademy.com.br>
2025-03-26 08:09:19 -0300

    Method & URL

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+  console.log(method, url);
   return res.end('Hello World');
 });
 
