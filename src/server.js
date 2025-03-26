0cc4bc696224b2140719824902a0377c2150b7aa
marcomaciel04 <marco.maciel@csacademy.com.br>
2025-03-26 08:12:46 -0300

    Múltiplas Rotas

--- src/server.js
+++ src/server.js
@@ -2,7 +2,15 @@ import http from 'node:http';
 
 const server = http.createServer((req, res) => {
   const { method, url } = req;
-  console.log(method, url);
+  
+  if(method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários');
+  }
+
+  if(method === 'POST' && url === '/users') {
+    return res.end('Criação de usuário');
+  }
+
   return res.end('Hello World');
 });
 
